e87091349a36f51c7c2bba75f6f2f63d7ac46377
illia-sh <Illia.Shpak@burberry.com>
2018-12-07 12:58:59 +0000

    portfix

--- AWS/sg-exposed-pub.py
+++ AWS/sg-exposed-pub.py
@@ -1,6 +1,5 @@
 import boto3
 import csv
-from collections import defaultdict
 
 ec2 = boto3.resource('ec2')
 
@@ -8,34 +7,29 @@ sg_groups = ec2.security_groups.filter(Filters=[{
  'Name': 'ip-permission.cidr',
  'Values': ['0.0.0.0/0']}])
 
-default = 0
-sg_info = defaultdict()
+sg_list = []
+
 for sg in sg_groups:
     for i in sg.ip_permissions:
         for ip in i['IpRanges']:
             if ip['CidrIp'] == '0.0.0.0/0':
                 if i.get('FromPort') != None:
                     if i['FromPort'] == -1 or i['ToPort'] == -1:
+                    #    print(i,i['FromPort'])
                         port = -1
                     else:
                         if i['FromPort'] == i['ToPort']:
+                        #    print(i,"TTEST",i['FromPort'])
                             port = i['FromPort']
                         else:
-                            port = (i['FromPort'] + ':' + i['ToPort'])
+                            port = (str(i['FromPort']) + ':' + str(i['ToPort']))
                 else:
                     port = 0
+                sg_list.append((sg.id,sg.group_name,port,sg.vpc_id))
 
-    # add securitygroup info
-    sg_info[sg.id] = {
-    'group_id': sg.id,
-    'group_name': sg.group_name,
-    'open_port': port,
-    'vpc_id' : sg.vpc_id
-    }
-
-with open('test.csv', mode='w') as csv_file:
-    fieldnames = ['group_id', 'group_name', 'open_port','vpc_id']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    for sg_id, sg in sg_info.items():
-        writer.writerow({'group_id': sg['group_id'], 'group_name': sg['group_name'],'open_port':sg['open_port'],"vpc_id":sg['vpc_id']})
+with open('sg_exposed_to_pub.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(("group_id", "group_name", "open_port", "vpc_id"))
+    for row in sg_list:
+        print(row)
+        writer.writerow(row)
